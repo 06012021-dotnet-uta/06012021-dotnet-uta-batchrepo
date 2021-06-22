34a8e385e4a13f7a5d5608844ec6650f2e9b2d25
Mark Moore <mark.moore@revature.com>
2021-06-22 12:13:50 -0500

    random

--- demos/RockPaperScissorsMvc/BusinessLayer/RpsGame.cs
+++ demos/RockPaperScissorsMvc/BusinessLayer/RpsGame.cs
@@ -19,8 +19,6 @@ namespace BusinessLayer
 		 * the parent variable only gives the user acces to the parent methods.
 		*/
 
-		//this is a ramdom change
-
 
 		private readonly RpsGameDb _context;
 		// create constructors.
